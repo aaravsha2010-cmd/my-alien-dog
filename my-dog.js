11c45f08a484ee4e4b8441b0f1fa11b3372e7768
aaravsha2010-cmd <aaravsha2010@gmail.com>
2026-01-31 18:10:37 +0000

    dog completed

---
+++ my-dog.js
@@ -0,0 +1,56 @@
+function setup() {
+  createCanvas(400, 400, WEBGL);
+}
+
+function draw() {
+  background(0, 255, 255);
+  // noStroke();
+  // background(red, green, blue);
+   fill(0, 250, 35)
+  circle(-25, -10, 75);
+  //
+  fill(80, 255, 45)
+  circle(-10, -10, 15);
+  //
+  fill(75, 255, 45)
+  circle(-40, -10, 15);
+  //circle(x-value, y-value, diameter)
+  
+  fill(300, 50, 35)
+ circle(10, 110, 35);
+   fill(300, 50, 35)
+ circle(55, 110, 35);
+   fill(300, 50, 35)
+ circle(100, 110, 35); 
+   fill(300, 50, 35)
+ circle(150, 110, 35);
+  //paws
+  
+  
+  
+   fill(0, 0, 255)
+  square(-10, 60, 40, 10, 20, 6, 7);
+   fill(0, 0, 255)
+  square(35, 60, 40, 10, 20, 6, 7);
+   fill(0, 0, 255)
+  square(80, 60, 40, 10, 20, 6, 7);
+   fill(0, 0, 255)
+  square(130, 60, 40, 10, 20, 6, 7);
+  // square(x-value, y-value, diameter);
+  
+
+  // triangle
+  
+
+  fill(255, 255, 0)
+  ellipse(70, 40, 220, 70);
+  ellipse(-70, -10, 25, 75);
+  ellipse(20, -10, 25, 75);
+  ellipse(-25, 10, 20, 20);
+ // ellipse(x,y,w,h, detail)
+  
+  
+  fill(0, 4, 250)
+  rect(-250, 125, 500, 120)
+
+}
